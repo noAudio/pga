ed6499ab87a876829abf59666d35da9e136e06cd
Samuel Odongo <samieodd@gmail.com>
2023-05-06 23:20:36 +0300

    Refactor validation

--- bin/pga.dart
+++ bin/pga.dart
@@ -6,9 +6,9 @@ void main(List<String> arguments) {
   String defaultLink = 'https://www.pgatour.com/leaderboard';
   String testLink =
       'https://www.pgatour.com/tournaments/2023/att-pebble-beach-pro-am/R2023005';
-  String? link = getUserInput(defaultLink);
+  String? link = getUserInput(testLink);
   if (link == null) {
-    print('User input skipped. Aborting process.');
+    print('⚠️ User input skipped. Aborting process.');
   } else {
     pga.navigateToPage(link);
   }
@@ -30,21 +30,20 @@ String? getUserInput(String defaultLink) {
   }
 
   if (link != '') {
-    if (validateLink(link)) {
-      if (link == defaultLink) {
-        print('Link matches default link.');
-        print('Navigating to default link ($defaultLink).');
-        return link;
-      }
-      print('Navigating to ($link).');
+    if (link == defaultLink) {
+      print('➡️ Link matches default link.');
+      print('Navigating to default link ($defaultLink).\n');
+      return link;
+    } else if (validateLink(link)) {
+      print('Navigating to ($link).\n');
       return link;
     } else {
-      print('Please enter a valid link from PGATour!');
-      print('You entered "$link".');
+      print(
+          '⚠️ Please enter a valid link from PGATour! You entered "$link".\n');
       return getUserInput(defaultLink);
     }
   } else {
-    print('No link provided. Navigating to default link ($defaultLink).');
+    print('➡️ No link provided. Navigating to default link ($defaultLink).');
     return defaultLink;
   }
 }
